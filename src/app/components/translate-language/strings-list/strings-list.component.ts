cb4c6739fcef3621002bd58ce5709259e668ae58
TomaszBachorski <tomekbachorski@o2.pl>
2022-07-25 21:45:56 +0200

    localstorage saving current filters

--- src/app/components/translate-language/strings-list/strings-list.component.ts
+++ src/app/components/translate-language/strings-list/strings-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
 import { stringsList } from 'src/app/customTypes';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
 import { StringsService } from 'src/app/services/strings-service.service';
 
 @Component({
@@ -19,19 +20,34 @@ export class StringsListComponent implements OnInit {
     };
 
     constructor(
-        private stringsService: StringsService
+        private stringsService: StringsService,
+        private localStorage: LocalStorageService
     ) { }
 
     ngOnInit(): void {
         this.stringsService.getStrings(this.language).subscribe((res: stringsList) => {
             this.stringsList = res;
         });
+
+        //loading previously saved options
+
+        Object.keys(this.statusCheckbox).forEach((key)=>{
+            if (this.localStorage.get(key)==="true") this.statusCheckbox[key as "pending" | "translated" | "approved"] = true;
+            if (this.localStorage.get(key)==="false") this.statusCheckbox[key as "pending" | "translated" | "approved"] = false;
+        })
+    }
+
+    //saving currently choosen filter options
+    @HostListener('window:unload')
+    private onUnload(): void {
+        Object.keys(this.statusCheckbox).forEach(key=>{
+            this.localStorage.set(key, String(this.statusCheckbox[key as "pending" | "translated" | "approved"]))
+        })
     }
 
     change(state: "pending" | "translated" | "approved"): void {
         document.querySelectorAll(`.${state}`).forEach((e)=>{
             this.statusCheckbox[state] = !this.statusCheckbox[state];
-            console.log(this.statusCheckbox)
             return e.classList.toggle(`display`)
         });
     }
